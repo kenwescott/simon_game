cf0eace59204ea2b85eb984b82288d99a5fa8f10
Emma-Christie Kameleu <emmakameleu@gmail.com>
2025-06-22 05:33:59 -0400

    instruction

--- simon_game/src/Page/Instructions.js
+++ simon_game/src/Page/Instructions.js
@@ -13,62 +13,33 @@ function MyVerticallyCenteredModal(props) {
       size="lg"
       aria-labelledby="contained-modal-title-vcenter"
       centered
-      
+      className="custom-modal"
     >
-      <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">
-          Game Instructions
-        </Modal.Title>
-      </Modal.Header>
+      <Modal.Header>
+  <Modal.Title id="contained-modal-title-vcenter">
+    Game Instructions
+  </Modal.Title>
+  <button className="custom-close-btn" onClick={props.onHide}>&times;</button>
+</Modal.Header>
+
+      
       <Modal.Body>
         <h4>How to Play</h4>
         <p>
-          Follow the pattern of lights and sounds. Click the buttons in the correct sequence to advance.
+          Follow the pattern of lights and sounds. 
         </p>
+        <p>Click the buttons in the correct sequence to advance.  </p> 
         <p>
           Each round gets more difficult. Good luck!
         </p>
       </Modal.Body>
       <Modal.Footer>
-        <Button variant="secondary" onClick={props.onHide}>Close</Button>
+        <Button className="custom-footer-close" onClick={props.onHide}>Close</Button>
       </Modal.Footer>
     </Modal>
   );
 }
 
-
-function Leveldropdown() {
-  return (
-    <Dropdown className='Level'>
-      <Dropdown.Toggle variant="success" id="dropdown-basic">
-        Level
-      </Dropdown.Toggle>
-
-      <Dropdown.Menu>
-        <Dropdown.Item href="#/action-1">Level 1</Dropdown.Item>
-        <Dropdown.Item href="#/action-2">Level 2</Dropdown.Item>
-        <Dropdown.Item href="#/action-3">Level 3</Dropdown.Item>
-      </Dropdown.Menu>
-    </Dropdown>
-
-  );
-}
-function difficultdropdown() {
-  return (
-    <Dropdown className='Difficulty'>
-      <Dropdown.Toggle variant="success" id="dropdown-basic">
-        Difficulty
-      </Dropdown.Toggle>
-
-      <Dropdown.Menu>
-        <Dropdown.Item href="#/action-1">Level 1</Dropdown.Item>
-        <Dropdown.Item href="#/action-2">Level 2</Dropdown.Item>
-        <Dropdown.Item href="#/action-3">Level 3</Dropdown.Item>
-      </Dropdown.Menu>
-    </Dropdown>
-
-  );
-}
 
 
 function Instruction() {
